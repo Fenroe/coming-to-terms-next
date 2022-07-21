584fd70d029dc8a641c6e4c955db015fc1f0631d
Fenroe <scrambles9494@gmail.com>
2022-07-22 00:16:11 +0300

    initial commit

---
+++ pages/banana.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+
+const Banana: NextPage = () => {
+  return (
+    <div>
+      <h1>Hello banana</h1>
+      <button className="btn-wide">Click me</button>
+    </div>
+  )
+}
+
+export default Banana
